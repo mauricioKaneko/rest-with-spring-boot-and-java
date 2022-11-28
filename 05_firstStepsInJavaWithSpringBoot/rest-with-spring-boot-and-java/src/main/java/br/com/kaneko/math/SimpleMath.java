53e3b52c59e99e0ded5ac3517498b814fda8714e
mauricioKaneko <mauricio.kaneko@gmail.com>
2022-11-28 09:21:13 -0300

    finalizando sessao

---
+++ 05_firstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/kaneko/math/SimpleMath.java
@@ -0,0 +1,23 @@
+package br.com.kaneko.math;
+
+public class SimpleMath {
+	public Double sum(Double numberOne,Double numberTwo) throws Exception {
+
+		return numberOne + numberTwo;
+	}
+
+	public Double subtract(Double numberOne,Double numberTwo) throws Exception {
+		
+		return numberOne - numberTwo;
+	}
+
+	public Double multiply(Double numberOne,Double numberTwo) throws Exception {
+
+		return numberOne * numberTwo;
+	}
+
+	public Double division(Double numberOne, Double numberTwo) throws Exception {
+
+		return numberOne / numberTwo;
+	}
+}
